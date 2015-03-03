b53ab6cf8d2f055fa301d67e039f36b35219d01e
Stuart Corcoran <stuart.corcoran@gmail.com>
2015-03-02 16:43:30 -0800

    changed cukerunner to TestRunner

---
+++ webtest/src/test/java/test_code/TestRunner.java
@@ -0,0 +1,17 @@
+package test_code;
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(
+		features = {"src/test/resources/"},
+		plugin = {"pretty", "html:target/cucumber-html-report", "junit:target/cucumber-junit-report/allcukes.xml"},
+		tags = {"@Runme"}
+)
+
+public class TestRunner {
+
+}
