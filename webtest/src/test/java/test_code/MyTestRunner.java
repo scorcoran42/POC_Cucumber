4ef7abf32443d6acbdb6ec3e14aafaa45f8ffa9d
Stuart Corcoran <stuart.corcoran@gmail.com>
2015-03-30 14:06:51 -0700

    added camelcase option

--- webtest/src/test/java/test_code/MyTestRunner.java
+++ webtest/src/test/java/test_code/MyTestRunner.java
@@ -1,7 +1,9 @@
 package test_code;
 
 import org.junit.runner.RunWith;
+
 import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
@@ -9,7 +11,8 @@ import cucumber.api.junit.Cucumber;
 @CucumberOptions(
 		features = {"src/test/resources/"},
 		plugin = {"pretty", "html:target/cucumber-html-report", "junit:target/cucumber-junit-report/allcukes.xml"},
-		tags = {"@Runme"}
+		tags = {"@Runme"},
+		snippets = SnippetType.CAMELCASE
 )
 
 public class MyTestRunner {
